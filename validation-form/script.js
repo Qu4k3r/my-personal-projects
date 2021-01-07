4a81445538289f5ff0be3b8670475f6f05b2d97b
Qu4k3r <neves0007@gmail.com>
2021-01-07 10:27:36 -0300

    script.js organized & input type radio retrieved in right position

--- validation-form/script.js
+++ validation-form/script.js
@@ -1,23 +1,17 @@
-// Step 2 & 3 - verify data format and throw error message
-function submitValues() {
-  const submitButton = document.querySelector('#submit-button')
-  submitButton.addEventListener('click', (event) => {
-    event.preventDefault()
-    deleteResultDiv()
-    showTextContent()
-    showSelectContent()
-  })
-}
-
-submitValues()
-
-// Step 3 & 4 - create a div that shows all the keys and values of data submitted
+//Step 3 & 4 - create a div that shows all the keys and values of data submitted
 function createDivArrays(array) {
   for (let key of array) {
     const resultDiv = document.createElement('div')
     resultDiv.className = 'result-div'
     resultDiv.innerText = `${key.value}`
     document.body.appendChild(resultDiv)
+  }
+}
+
+function deleteResultDiv() {
+  const myArrayDiv = document.querySelectorAll('.result-div')
+  for (const div of myArrayDiv) {
+    div.remove()
   }
 }
 
@@ -28,22 +22,45 @@ function createSimpleDiv(element) {
   return resultDiv
 }
 
+//Text content
 function showTextContent() {
   const arrayOfResults = document.querySelectorAll('input[type=text]')
   createDivArrays(arrayOfResults)
 }
 
+//Select value
 function showSelectContent() {
   const mySelectValue = document.querySelector('select').value
   const resultDiv = createSimpleDiv(mySelectValue)
   document.body.insertBefore(resultDiv, document.body.childNodes[18])
 }
 
-function deleteResultDiv() {
-  const myArrayDiv = document.querySelectorAll('.result-div')
-  for (let div of myArrayDiv) {
-    div.remove()
-  }
+//type=radio, textarea, type=date
+function showRadioValue () {
+  const myOptions = document.querySelectorAll('input[type=radio]')
+  for (const option of myOptions) {
+    if (option.checked) {
+      const resultDiv =  createSimpleDiv(option.value)
+      document.body.insertBefore(resultDiv, document.body.childNodes[19])
+    }
+  } 
 }
 
-// select, type=radio, textarea, type=date
+//Textarea content
+function showTextAreaContent() {}
+
+//Date Value
+function showDateValue() {}
+
+function submitValues() {
+  const submitButton = document.querySelector('#submit-button')
+  submitButton.addEventListener('click', (event) => {
+    event.preventDefault()
+    deleteResultDiv()
+    showTextContent()
+    showSelectContent()
+    showRadioValue()
+  })
+}
+
+submitValues()
